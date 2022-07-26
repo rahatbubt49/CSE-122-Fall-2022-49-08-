b8557cc13d3eec26a94dfc1c12f3115d5c6744f3
Rahat Bin Aowlad <109545385+rahatbubt49@users.noreply.github.com>
2022-07-26 11:01:32 +0600

    Add files via upload

---
+++ Lab 02/5.336_49-08.cpp
@@ -0,0 +1,15 @@
+// Codeforces Problem No. - 50A
+// Problem Name - Domino Piling
+
+#include <iostream>
+using namespace std;
+int main(){
+    int m,n,s;
+    cin>>m>>n;
+    if(m>=1 && n<=16)
+    {
+        s=(m*n)/2;
+    }
+    cout<<s<<endl;
+
+}
